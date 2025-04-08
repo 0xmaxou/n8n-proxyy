9f1e685dbdf08dee570245f8e3a238f602fc54f1
maxsouv80 <maxsouv80@gmail.com>
2025-04-08 12:37:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# n8n-proxyy
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/0xmaxou/n8n-proxyy)
---
+++ proxy-n8n.js
@@ -0,0 +1,22 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).send("Method not allowed");
+  }
+
+  const webhookURL = "https://TON_WEBHOOK.n8n.cloud/webhook/TON_ENDPOINT"; // Remplace par ton vrai lien
+
+  try {
+    const response = await fetch(webhookURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req.body),
+    });
+
+    const data = await response.text();
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send("Proxy error");
+  }
+}
